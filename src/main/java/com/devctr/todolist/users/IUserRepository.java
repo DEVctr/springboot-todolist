08519f699d3a9058f32376e7d52b7e0f84558f13
DEVctr <vferreira487@gmail.com>
2023-10-15 11:45:22 -0300

    Dockerfile deploy

---
+++ src/main/java/com/devctr/todolist/users/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.devctr.todolist.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+   UserModel findByUsername(String username);
+}
